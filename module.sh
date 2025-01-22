24ad8ca351a16a1b3f9bff5c08a7b84c05399ca3
danu99721 <danuprasetyo116@gmail.com>
2025-01-22 15:14:53 +0700

    Update module.sh

--- module.sh
+++ module.sh
@@ -1,6 +1,15 @@
+echo "install module....."
+sleep 0.5
+clear
 pip install  time
 pip install requests
 pip install subprocess
 pip install psutil
 
-git clone 
+clear
+
+rm -rf xion 
+clear
+git clone https://github.com/danu99721/xion
+cd xion 
+python main.py
